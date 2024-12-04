e02aae1b4f91f0ae64e8e03565576e0538572b6d
RAVanGorder <ravangorder@gmail.com>
2024-12-04 11:11:37 -0500

    Update Apical_Solver.m

--- Apical_Solver.m
+++ Apical_Solver.m
@@ -1,25 +1,25 @@
 function Apical_Solver
-% Function produces the plots for the apical growths (solve the PDE + numerical modes + linear system)
+% Function produces the plots for the apical growths (solve the PDE & extract numerical modes & solve the linear instability system)
 
 %% Parameters
-% Params for model
+% Parameters for reaction-diffusion system
 a=0.01;
 b=1;
 d_1 = 0.004;
 d_2 = 0.1;
 L=1;
 
-% Params for solution
+% Parameters for simulation
 Tf=100;
 max_mode = 60;
 kappa = max_mode+1;
 x = linspace(0,1,501);
 t = linspace(0,Tf,1001);
 
-% Inital conditions for linear ODE system
+% Inital conditions for linear ODE system governing instability
 IC1 = 0.0001*(rand(2,kappa)-0.5);
 
-%% Solve PDE
+%% Solve the PDE system
 
 function zvec = kenetics(w)
     zvec = [a-w(1) + w(1)^2 * w(2); b - w(1)^2 * w(2)];
@@ -33,7 +33,6 @@ function u0 = pdeic(xx) % Reconstruct appropriate ICs from mode ICs
 end
 
 function [c,f,s] = pdefun(x,t,w,dw)
-
     c = [1;1];
     f = (1/r(t).^2)*[d_1; d_2].* dw;
     s = kenetics(w) + x*rdot(t)*dw/r(t);
@@ -50,7 +49,7 @@ sol = pdepe(0, @pdefun, @pdeic, @pdebc, x, t);
 u = sol(:,:,1);
 v = sol(:,:,2);
 
-%% Plot solution
+%% Plot the solution, u, of the activator in the reaction-diffusion system
 
 figure('Color','white')
 
@@ -66,7 +65,7 @@ title('System evolution', Interpreter='latex', FontSize=20)
 colormap(ax1,parula)
 shading interp
 
-%% Plot numeric modes over time
+%% Plot numerically computed modes over time
 
 krange = 1:max_mode;
 mag_matrix = zeros(length(t),max_mode);
@@ -89,20 +88,19 @@ ylabel('Time $t$', Interpreter='latex', FontSize=18)
 title('Numerically extracted modes', Interpreter='latex', FontSize=20)
 shading flat
 
-%% Plot predicted mode growth over time (Solve truncated ODE system)
+%% Solve the linear instability system, a system of ODEs
 
 J = [(b-a)/(a+b), (a+b)^2; -2*b/(a+b), -(a+b)^2];
 Dif = [d_1, 0; 0, d_2];
 [time_modes,curr_sol] = ode45(@(t,kvec) derivModes(t,kvec,J,Dif,kappa,L), [0,Tf], IC1);
 
-% Convert solution into relative magnitudes over time
+% Convert the ODE solutions into relative magnitudes over time
 sol = reshape(curr_sol(:,:),[length(time_modes),2,kappa]);
 mags = squeeze(sqrt(sol(:,1,:).^2+sol(:,2,:).^2));
 mags_max = max(mags,[],2);
 rel_mags = mags./mags_max;
 
-
-% Plot
+% Plot the ODE solutions
 [K,T] = meshgrid(krange,time_modes);
 ax3 = subplot(1,3,3);
 
@@ -141,7 +139,7 @@ function D = derivModes(t,kvec,J,Dif,kappa,L) % RHS of Eqn (5.14) in Thm 5
     D = reshape(D, [2*kappa,1]);
 end
 
-% Prescribed domain growth (and deriv)
+% Prescribed domain growth (and rates of change)
 function z = r(t)
     z = 1+0.05*t;
 end
@@ -150,7 +148,7 @@ function z = rdot(t)
     z = 0.05; 
 end
 
-% Coefficents in Eqn (5.14) series
+% Coefficents in series expansion for linear instability result
 function M = getCoefs(max)
     M = zeros(max-1,max-1);
 
